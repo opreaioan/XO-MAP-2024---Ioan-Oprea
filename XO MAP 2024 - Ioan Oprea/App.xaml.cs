de79a2e30270472c59121408ac5b54a0e7e6fac0
opreaioan <oprea.ioan@ymail.com>
2024-03-19 06:45:22 +0200

    Add project files.

---
+++ XO MAP 2024 - Ioan Oprea/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace XO_MAP_2024___Ioan_Oprea
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
