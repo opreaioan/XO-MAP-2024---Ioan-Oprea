140dbf66879e2f5fb2fe8f4de10c97cea2b4b153
opreaioan <oprea.ioan@ymail.com>
2024-03-20 23:16:11 +0200

    final simple working tested

--- XO MAP 2024 - Ioan Oprea/MainWindow.xaml.cs
+++ XO MAP 2024 - Ioan Oprea/MainWindow.xaml.cs
@@ -123,8 +123,12 @@ namespace XO_MAP_2024___Ioan_Oprea
         /// </summary>
         private void CheckForWinner()
         {
+
             // Check for horizontal wins
-            if (mResults[0] != MarkType.Free && (mResults[1] & mResults[2]) == mResults[0])
+            //
+            //  - Row 0
+            //  
+            if (mResults[0] != MarkType.Free && (mResults[0] & mResults[1] & mResults[2]) == mResults[0])
             {
                 // Game ends
                 mGameEnded = true;
@@ -132,10 +136,92 @@ namespace XO_MAP_2024___Ioan_Oprea
                 // Highlight winning cells in green
                 Button0_0.Background = Button1_0.Background = Button2_0.Background = Brushes.Green;
             }
+            //
+            //  - Row 1
+            //  
+            if (mResults[3] != MarkType.Free && (mResults[3] & mResults[4] & mResults[5]) == mResults[3])
+            {
+                // Game ends
+                mGameEnded = true;
 
+                // Highlight winning cells in green
+                Button0_1.Background = Button1_1.Background = Button2_1.Background = Brushes.Green;
+            }
+            //
+            //  - Row 2
+            //  
+            if (mResults[6] != MarkType.Free && (mResults[6] & mResults[7] & mResults[8]) == mResults[6])
+            {
+                // Game ends
+                mGameEnded = true;
+
+                // Highlight winning cells in green
+                Button0_2.Background = Button1_2.Background = Button2_2.Background = Brushes.Green;
+            }
+
+            // Check for vertical wins
+            //
+            //  - Col 0
+            //  
+            if (mResults[0] != MarkType.Free && (mResults[0] & mResults[3] & mResults[6]) == mResults[0])
+            {
+                // Game ends
+                mGameEnded = true;
+
+                // Highlight winning cells in green
+                Button0_0.Background = Button0_1.Background = Button0_2.Background = Brushes.Green;
+            }
+
+            //
+            //  - Col 1
+            //  
+            if (mResults[1] != MarkType.Free && (mResults[1] & mResults[4] & mResults[7]) == mResults[1])
+            {
+                // Game ends
+                mGameEnded = true;
+
+                // Highlight winning cells in green
+                Button1_0.Background = Button1_1.Background = Button1_2.Background = Brushes.Green;
+            }
+
+            //
+            //  - Col 2
+            //  
+            if (mResults[2] != MarkType.Free && (mResults[2] & mResults[5] & mResults[8]) == mResults[2])
+            {
+                // Game ends
+                mGameEnded = true;
+
+                // Highlight winning cells in green
+                Button2_0.Background = Button2_1.Background = Button2_2.Background = Brushes.Green;
+            }
+
+            //
+            //  - Diagonal 1
+            //  
+            if (mResults[0] != MarkType.Free && (mResults[0] & mResults[4] & mResults[8]) == mResults[0])
+            {
+                // Game ends
+                mGameEnded = true;
+
+                // Highlight winning cells in green
+                Button0_0.Background = Button1_1.Background = Button2_2.Background = Brushes.Green;
+            }
+            
+            //
+            //  - Diagonal 2
+            //  
+            if (mResults[2] != MarkType.Free && (mResults[2] & mResults[4] & mResults[6]) == mResults[2])
+            {
+                // Game ends
+                mGameEnded = true;
+
+                // Highlight winning cells in green
+                Button2_0.Background = Button1_1.Background = Button0_2.Background = Brushes.Green;
+            }
 
             // Check for no winner and full board
-            if (!mResults.Any(result => result == MarkType.Free))
+            if ((!mGameEnded)&&(!mResults.Any(result => result == MarkType.Free)))
             {
                 // Game ended
                 mGameEnded = true;
@@ -145,7 +231,8 @@ namespace XO_MAP_2024___Ioan_Oprea
                 {
                     button.Background = Brushes.Orange;
                 });
-            }   
+            }
+
         }
 
     }
